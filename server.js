392309b09f5d03556ba3a7564e5e2828dff9a6e2
Evaldas <evaldaskal@gmail.com>
2016-10-30 22:56:34 +0200

    some initial files

---
+++ server.js
@@ -0,0 +1,12 @@
+//Server code
+
+// modules
+
+var express = require('express');
+var app = express();
+var bodyParser =  require('body-parser');
+var methodOverride = require('method-override');
+
+//configuration
+
+//config files
