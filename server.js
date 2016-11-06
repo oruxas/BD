9e39a19345c5e68222cb403f77510e6e10c59dbd
Evaldas <evaldaskal@gmail.com>
2016-11-06 12:34:17 +0200

    server.js setup

--- server.js
+++ server.js
@@ -10,3 +10,42 @@ var methodOverride = require('method-override');
 //configuration
 
 //config files
+
+var db = require('./config/db');
+
+//port set up
+var port = process.env.PORT || 8080;
+
+//uncomment after entering credentials in config/db.js
+//mongoose.connect(db.url);
+
+//get all data/stuff of the body (POST) parameters
+//parse application/json
+
+app.use(bodyParser.json());
+
+//parse application/x-www-form-urlencencoded
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+//override with the X-HTTP-Methd-Override header in the request.  simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+//set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+//ROUTES ===========================================================================
+require('./app.routes')(app); //configure routes
+
+//START APP ========================================================================
+
+//startup our app at http http://localhost:8080
+app.listen(port);
+
+//checking
+concole.log('Prisijunges prie port\'o: ' + port);
+
+//expose appexports = module.exports = app
+
+
