9ef077dac5a0d6aca6109353229b940f8b90d9c4
Evaldas <evaldaskal@gmail.com>
2016-11-22 19:26:46 +0200

    updated Schema

--- app/models/workoutPlan.js
+++ app/models/workoutPlan.js
@@ -6,10 +6,34 @@ var mongoose = require('mongoose');
 //define our models
 //module.exports allows to pass this to other files when called
 
-module.exports = mongoose.model('workoutPlan', {
-    workoutType: {
-        bodyweight: {type: String, default: ''},
-        weights: {type: String, default: ''},
-        mixed: {type: String, default: ''}
+module.exports = mongoose.model('workoutPlan', [
+    {
+        userId : Number,
+        userName : String,
+        password : String
+    },
+    {
+        workoutType: {
+            bodyweight: [   {totalDuration : Date}, 
+                            {daysPerWeek : Date}, 
+                            {bodyPart : String}, 
+                            {selectedExercise : String}, 
+                            {sets : Number}, 
+                            {reps : Number}
+                        ],
+            weights: [      {totalDuration : Date}, 
+                            {daysPerWeek : Date}, 
+                            {bodyPart : String}, 
+                            {selectedExercise : String}, 
+                            {sets : Number}, 
+                            {reps : Number}
+                        ],
+            mixed: [        {totalDuration : Date}, 
+                            {daysPerWeek : Date}, 
+                            {bodyPart : String}, 
+                            {selectedExercise : String}, 
+                            {sets : Number}, 
+                            {reps : Number}
+                        ]
     }
-});
+}]);
